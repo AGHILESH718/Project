51c51f19966c5b784db9e78cdf8fc9a51f97128a
AGHILESH718 <aghilesh2003@gmail.com>
2024-12-30 13:26:47 +0530

    commit changes

--- Project_11/script.js
+++ Project_11/script.js
@@ -1,14 +1,16 @@
 const jokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
-const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
-let getJoke = () =>{
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = () => {
     jokeContainer.classList.remove("fade");
     fetch(url)
     .then(data => data.json())
-    .then(item =>{
-        jokeContainer.textContent = '${item.joke}';
+    .then(item => {
+        jokeContainer.textContent = `${item.joke}`; // Corrected template literal
         jokeContainer.classList.add("fade");
     });
-}
-btn.addEventListener("click",getJoke);
+};
+
+btn.addEventListener("click", getJoke);
 getJoke();
